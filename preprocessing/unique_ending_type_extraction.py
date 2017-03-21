fd7679a363f4582f389653f7da7050746f686637
Jeroen Stravers <jeroen.stravers@live.nl>
2017-03-21 12:05:33 +0100

    cleaned up unique_ending_type_extraction code

--- preprocessing/unique_ending_type_extraction.py
+++ preprocessing/unique_ending_type_extraction.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("../")
 import game_ending_type_extraction as g_e_t_extraction
+import utils
 
 
 class UniqueEndingTypeIdentifier:
@@ -13,35 +14,38 @@ class UniqueEndingTypeIdentifier:
         self._encountered_ending_types = dict()
         self._main_log_directory = target_path
 
-    def store_paths_to_logs_of_unique_game_endings(self, path_to_log, ending_type):
+    def display_examples_of_unique_game_endings(self, game_endings_with_paths):
+        """
+        display the results in the console, sorted ascending
+        """
+        self._store_paths_to_logs_of_unique_game_endings(game_endings_with_paths)
+        sorted_ending_types = sorted(self._encountered_ending_types.keys())
+        for ending_type in sorted_ending_types:
+            print(ending_type)
+            print(self._encountered_ending_types[ending_type])
+
+    def _store_paths_to_logs_of_unique_game_endings(self, game_endings_with_paths):
         """
         store the game ending type and the path to an xml containing an example of this ending (relative to the main
         game log directory) in self._encountered_ending_types
         :param path_to_log: the path to the current game log being processed
         :param ending_type: the numerical type of game ending
         """
-        if ending_type not in self._encountered_ending_types:
-            cleaned_path_to_log = path_to_log.replace(self._main_log_directory, "")
-            self._encountered_ending_types[ending_type] = cleaned_path_to_log
-
-    def display_examples_of_unique_game_endings(self):
-        """
-        display the results in the console, sorted ascending
-        """
-        sorted_ending_types = sorted(self._encountered_ending_types.keys())
-        for ending_type in sorted_ending_types:
-            print(ending_type)
-            print(self._encountered_ending_types[ending_type])
+        for game_log_subdir_processor in game_endings_with_paths:
+            for ending_type, log_path in game_log_subdir_processor:
+                if ending_type not in self._encountered_ending_types:
+                    log_name = utils.get_item_by_index_from_string_split_on(
+                        utils.LAST_ITEM_IN_LIST,
+                        log_path,
+                        utils.FORWARD_SLASH)
+                    self._encountered_ending_types[ending_type] = log_name
 
 
 if __name__ == "__main__":
     #path = "C:/Users/Jeroen/Desktop/Stratego games-20170320T133045Z-001/Stratego games"
     path = "C:/Users/Jeroen/Desktop/Stratego games-20170320T133045Z-001/Cleaned_Stratego_Games"
     extractor = g_e_t_extraction.GameEndingTypeExtractor(path)
+    game_endings_with_paths = extractor.get_game_endings()
+
     type_identifier = UniqueEndingTypeIdentifier(path)
-
-    for game_log_subdir_processor in extractor.get_game_endings():
-        for ending_type, log_path in game_log_subdir_processor:
-            type_identifier.store_paths_to_logs_of_unique_game_endings(log_path, ending_type)
-
-    type_identifier.display_examples_of_unique_game_endings()
+    type_identifier.display_examples_of_unique_game_endings(game_endings_with_paths)
